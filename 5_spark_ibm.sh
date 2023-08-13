9b169f99b2061f6aa1f470d002dff5a79f8aa311
jose.alvarez <jose.alvarez@tata.com.uy>
2023-08-13 16:07:36 +0200

    ibm

---
+++ 5_spark_ibm.sh
@@ -0,0 +1,16 @@
+# preparation
+
+#python3 -m pip install pyspark
+
+git clone https://github.com/big-data-europe/docker-spark.git
+
+cd docker-spark
+
+docker-compose up
+
+# create submit.py file
+
+# execute python 
+python3 submit.py
+
+#http://localhost:8081/
